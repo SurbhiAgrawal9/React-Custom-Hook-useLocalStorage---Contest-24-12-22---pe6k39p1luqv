bdb9cc9ef796999818a67a6ef2a5aa6450742024
Surbhi Agrawal <111246726+SurbhiAgrawal9@users.noreply.github.com>
2022-12-24 16:30:18 +0530

    Update useLocalStorage.js

--- src/components/useLocalStorage.js
+++ src/components/useLocalStorage.js
@@ -5,11 +5,10 @@ const useLocalStorage = (stateVar, value) => {
   const[curState,setState] = useState(value);
   updateStorage(stateVar,curState);
   return [curState,setState];
-  
 };
 
-const updateStorage = (key,val)=>{
-  localStorage.setitem(key,val);
+const updateStorage =(key,val)=>{
+    localStorage.setItem(key,val);
 }
 
 export default useLocalStorage;
